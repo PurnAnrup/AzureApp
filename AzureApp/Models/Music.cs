8364c4a56fe07598b7a6de427bf8b1bcd696a949
purna anrup <purnaanrup100@gmail.com>
2021-09-16 12:47:57 +0530

    push

---
+++ AzureApp/Models/Music.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureApp.Models
+{
+    public class Music
+    {
+        public int Id { get; set; }
+        public string SongName { get; set; }
+        public string SongGenre { get; set; }
+        public string Singer { get; set; }
+    }
+}
---
+++ AzureApp/Models/MusicDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureApp.Models
+{
+    public class MusicDbContext : DbContext
+    {
+        public DbSet<Music> Musics {get; set;}
+        public MusicDbContext(DbContextOptions<MusicDbContext> options):base(options)
+        {
+
+        }
+    }
+}
